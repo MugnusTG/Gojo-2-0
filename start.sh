e8c7c99a9e91b48ea7a4cd292340b425c53a88ee
MagnusTG <93865402+ateinstein12@users.noreply.github.com>
2021-11-15 10:37:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/ateinstein12/Gojo-2-0.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
